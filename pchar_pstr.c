610935751d4710346f8396226ffccc01a2b2ea83
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-17 18:48:42 +0200

    "modify pchar func"

--- pchar_pstr.c
+++ pchar_pstr.c
@@ -13,7 +13,7 @@ void pchar_el(stack_t **stack, unsigned int line_number)
 	}
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
-		fprintf(stderr, "L%d can't pchar, value out of range\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
