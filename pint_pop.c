9c86f01a16bd1aba1163a17751254ecfe8e4bb9c
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-17 17:44:06 +0200

    "modify pint fun"

--- pint_pop.c
+++ pint_pop.c
@@ -8,7 +8,7 @@ void p_int(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "Error: L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
